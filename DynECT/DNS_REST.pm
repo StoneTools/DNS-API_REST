a1fe97256349962025e457983a2492f695662589
Ryan D. Brickett <rbrickett@dyn.com>
2013-09-20 13:23:33 -0400

    Added support for API Version, improvements to login
    Moved ->login to use standard request method
    API version support added in declaration and procedually

--- DynECT/DNS_REST.pm
+++ DynECT/DNS_REST.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
-#Version 1.01.01
+$VERSION = "1.01";
+$VERSION = eval $VERSION;
 
 package DynECT::DNS_REST;
 
@@ -8,24 +9,37 @@ use strict;
 use warnings;
 use LWP::UserAgent;
 use JSON;
+use Data::Dumper;
 
 #TODO: Write documentation with POD
 #TODO: Add support for API version declaration and use
+#TODO: Add keepalive
+
 #Constructor
 sub new {	
 	#reference to self if first argument passed in
 	my $classid = shift;
+	my %con_args = @_;
 	
 	my $self = {
 		#LWP User agent instance
 		lwp => '',		
-		apitoken => '',
+		apitoken => undef,
+		apiver => undef,
 		#Current status meesage
 		message => '',
 		#Reference to a hash for JSON decodes of most recent result
 		resultref => '',
 	};
 
+	$$self{'apiver'} = $con_args{ 'version' } if ( exists $con_args{ 'version' } );
+
+	$$self{'lwp'} = LWP::UserAgent->new;
+	#diable redirect following as that is a special case with DynECT
+	$$self{'lwp'}->max_redirect( '0' );
+	#reduce timeout from 180 seconds to 20
+	$$self{'lwp'}->timeout( '20' );
+	
 	bless $self, $classid;
 
 	return $self;
@@ -38,34 +52,18 @@ sub login {
 	my ($classid, $custn, $usern, $pass) = @_;
 
 	#API login
-	my $session_uri = 'https://api.dynect.net/REST/Session';
 	my %api_param = (
 		'customer_name' => $custn,
 		'user_name' => $usern,
 		'password' => $pass,
 	);
 
-	my $api_request = HTTP::Request->new('POST','https://api.dynect.net/REST/Session');
-	$api_request->header ( 'Content-Type' => 'application/json' );
-	$api_request->content( to_json( \%api_param ) );
-
-	$classid->{'lwp'} = LWP::UserAgent->new;
-	#diable redirect following as that is a special case with DynECT
-	$classid->{'lwp'}->max_redirect( '0' );
-
-	my $api_result = $classid->{'lwp'}->request( $api_request );
-
-	#check if call succeeded
-	my $res = $classid->check_res( $api_result );
+	my $res = $classid->request( 'OVERRIDESESSION', 'POST', \%api_param);
 	if ( $res ) {
-		#Grab API key
-		$classid->{'apitoken'} = $classid->{'resultref'}->{'data'}->{'token'};
-		$classid->{'message'} = "Login successful";
-		return 1;
+		$$classid{'apitoken'} = $$classid{'resultref'}{'data'}{'token'};
+		$$classid{'message'} = 'Session successfully created';
 	}
-
 	return $res;
-
 }
 
 sub logout {
@@ -73,15 +71,15 @@ sub logout {
 	my $classid = shift;
 	#TODO: Set message if API token not set
 	#existance of the API key means we are logged in
-	if ( $classid->{'apitoken'} ) {
+	if ( $$classid{'apitoken'} ) {
 		#Logout of the API, to be nice
 		my $api_request = HTTP::Request->new('DELETE','https://api.dynect.net/REST/Session');
-		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apitoken'} );
-		my $api_result = $classid->{'lwp'}->request( $api_request );
+		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
+		my $api_result = $$classid{'lwp'}->request( $api_request );
 		my $res =  $classid->check_res( $api_result );
 		if ( $res ) {
-			undef $classid->{'apitoken'};
-			$classid->{'message'} = "Logout successful";
+			undef $$classid{'apitoken'};
+			$$classid{'message'} = "Logout successful";
 		}
 		
 		return $res;
@@ -93,36 +91,48 @@ sub request {
 	if (defined $paramref) {
 		#weak check for correct paramater type
 		unless ( ref($paramref) eq 'HASH' ) {
-			$classid->{'message'} = "Invalid paramater type.  Please utilize a hash reference";
+			$$classid{'message'} = "Invalid paramater type.  Please utilize a hash reference";
 			return 0;
 		}
 	}
-	#TODO: Check for keepalive
+
 	if ( $uri =~ /\/REST\/Session/ ) {
-		$classid->{'message'} = "Please use the ->login or ->logout for managing sessions";
+		$$classid{'message'} = "Please use the ->login, ->keepalive, or ->logout for managing sessions";
 		return 0;
 	}
+
+	#catch internal use of session URI
+	$uri = '/REST/Session/' if ( $uri eq 'OVERRIDESESSION' ); 
 
 	#weak check for valid URI
-	#TODO: Set this to detect start of string
-	#TODO: Clean up this logic
-	if ( !($uri =~ /\/REST\// ) ) {
-		$classid->{'message'} = "Invalid REST URI.  Correctly formatted URIs start with '/REST/";
+	unless ( $uri =~ /^\/REST\// ) {
+		$$classid{'message'} = "Invalid REST URI.  Correctly formatted URIs start with '/REST/";
 		return 0;
 	}
 
-	my $api_request = HTTP::Request->new(uc($method), "https://api.dynect.net$uri");
-	$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apitoken'} );
+	#Check for valid method type
+	$method = uc( $method );
+	print "\n\n\$URI = $uri\n\n";
+	unless ( $method eq 'GET' || $method eq 'POST' || $method eq 'PUT' || $method eq 'DELETE' ) {
+		$$classid{ 'message' } = 'Invalid method type.  Please use GET, PUT, POST, or DELETE.';
+		return 0;
+	}
+
+	my $api_request = HTTP::Request->new( $method , "https://api.dynect.net$uri");
+	$api_request->header ( 'Content-Type' => 'application/json' );
+	$api_request->header ( 'Auth-Token' => $$classid{'apitoken'} ) if ( defined $$classid{'apitoken'} );
+	$api_request->header ( 'Version' => $$classid{'apiver'} ) if ( defined $$classid{'apiver'} );
+
 	if (defined $paramref) {
 		$api_request->content( to_json( $paramref ) );
 	}
 
-	my $api_result = $classid->{'lwp'}->request( $api_request );
+	my $api_result = $$classid{'lwp'}->request( $api_request );
 	#check if call succeeded
 	my $res =  $classid->check_res( $api_result );
 
 	# If $res, set the message
-	$classid->{'message'} = "Request ( $uri, $method) successful" if $res;
+	$$classid{'message'} = "Request ( $uri, $method) successful" if $res;
 	
 	return $res;
 }
@@ -134,7 +144,7 @@ sub check_res {
 	
 	#Fail out if there is no content in the response
 	unless ($api_result->content) { 
-		$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+		$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 		return 0;
 	}
 
@@ -143,27 +153,27 @@ sub check_res {
 	if ($api_result->code == 307) {
 		sleep 2;
 		my $api_request = HTTP::Request->new('GET', "https://api.dynect.net" . $api_result->content);
-		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apitoken'} );
-		$api_result = $classid->{'lwp'}->request( $api_request );
+		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
+		$api_result = $$classid{'lwp'}->request( $api_request );
 		unless ( $api_result->content ) { 
-			$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+			$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 			return 0;
 		}
 	}
 
 	#now safe to decode JSON
-	$classid->{'resultref'} = decode_json ( $api_result->content );
+	$$classid{'resultref'} = decode_json ( $api_result->content );
 
 	#loop until the job id comes back as success or program dies
 	while ( $$classid{'resultref'}{'status'} ne 'success' ) {
 		if ( $$classid{'resultref'}{'status'} ne 'incomplete' ) {
 			#api stauts != sucess || incomplete would indicate an API failure
-			foreach my $msgref ( @{$classid->{'resultref'}->{'msgs'}} ) {
-				$classid->{'message'} = "API Error:\n";
-				$classid->{'message'} .= "\tInfo: $msgref->{'INFO'}\n" if $msgref->{'INFO'};
-				$classid->{'message'} .= "\tLevel: $msgref->{'LVL'}\n" if $msgref->{'LVL'};
-				$classid->{'message'} .= "\tError Code: $msgref->{'ERR_CD'}\n" if $msgref->{'ERR_CD'};
-				$classid->{'message'} .= "\tSource: $msgref->{'SOURCE'}\n" if $msgref->{'SOURCE'};
+			foreach my $msgref ( @{$$classid{'resultref'}->{'msgs'}} ) {
+				$$classid{'message'} = "API Error:\n";
+				$$classid{'message'} .= "\tInfo: $msgref->{'INFO'}\n" if $msgref->{'INFO'};
+				$$classid{'message'} .= "\tLevel: $msgref->{'LVL'}\n" if $msgref->{'LVL'};
+				$$classid{'message'} .= "\tError Code: $msgref->{'ERR_CD'}\n" if $msgref->{'ERR_CD'};
+				$$classid{'message'} .= "\tSource: $msgref->{'SOURCE'}\n" if $msgref->{'SOURCE'};
 			};
 			return 0;
 		}
@@ -175,25 +185,32 @@ sub check_res {
 			$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
 			my $api_result = $$classid{'lwp'}->request( $api_request );
 			unless ( $api_result->content ) { 
-				$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+				$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 				return 0;
 			}
-			$classid->{'resultref'} = decode_json( $api_result->content );
+			$$classid{'resultref'} = decode_json( $api_result->content );
 		}
 	}
 	
 	return 1;
 }
 
+sub version {
+	my $classid = shift;
+	my $ver = shift;
+	$$classid{'apiver'} = $ver if ( defined $ver );
+	return $$classid{'apiver'};
+}
+
 
 sub message {
 	my $classid = shift; 
-	return $classid->{'message'};
+	return $$classid{'message'};
 }
 
 sub result {
 	my $classid = shift;
-	return $classid->{'resultref'};
+	return $$classid{'resultref'};
 }
 
 sub DESTROY {
